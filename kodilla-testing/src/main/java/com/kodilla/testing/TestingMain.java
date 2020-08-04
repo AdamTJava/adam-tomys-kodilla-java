5766ac47c5ab0b0b6843551e0b4ab4f2e0333c1c
AdamTJava <adamtomys@gmail.com>
2020-08-04 18:24:46 +0200

    added ForumTestSuite

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,30 +1,7 @@
 package com.kodilla.testing;
 
-import com.kodilla.testing.calculator.Calculator;
-import com.kodilla.testing.user.SimpleUser;
-
 public class TestingMain {
     public static void main(String[] args) {
-        SimpleUser simpleUser = new SimpleUser("Adam");
-
-        String result = simpleUser.getUsername();
-
-        if (result.equals("Adam")) {
-            System.out.println("Test OK");
-        }else {
-            System.out.println("Error");
-        }
-
-        Calculator calculator = new Calculator();
-
-        int addResult = calculator.addAToB(2,7);
-        int substractResult = calculator.substractBFromA(9,5);
-
-        if (addResult == 9 && substractResult == 4) {
-            System.out.println("Test OK");
-        }else {
-            System.out.println("Error");
-        }
 
     }
 }
