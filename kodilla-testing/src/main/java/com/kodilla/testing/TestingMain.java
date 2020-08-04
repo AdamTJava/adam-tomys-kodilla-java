0813dfc7c96380279db012d78b1871089da0cd16
AdamTJava <adamtomys@gmail.com>
2020-08-04 14:57:47 +0200

    Module 6 second task

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,33 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("Adam");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("Adam")) {
+            System.out.println("Test OK");
+        }else {
+            System.out.println("Error");
+        }
+
+        Calculator calculator = new Calculator();
+
+        if (calculator.addAToB(2,7) == 9) {
+            System.out.println("test OK");
+        }else {
+            System.out.println("Error");
+        }
+
+        if (calculator.substractBFromA(9,5) == 4) {
+            System.out.println("test OK");
+        }else {
+            System.out.println("Error");
+        }
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public int addAToB(int a, int b) {
+        return a + b;
+    }
+
+    public int substractBFromA(int a, int b) {
+        return a - b;
+    }
+
+}
