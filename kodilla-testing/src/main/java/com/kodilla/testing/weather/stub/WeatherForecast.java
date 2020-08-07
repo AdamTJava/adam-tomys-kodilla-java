aa9e017c9511b439be0ac01c038110530b14aca5
AdamTJava <adamtomys@gmail.com>
2020-08-07 16:34:23 +0200

    Module 6 task 5

--- kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
@@ -1,7 +1,6 @@
 package com.kodilla.testing.weather.stub;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class WeatherForecast {
 
@@ -17,6 +16,34 @@ public class WeatherForecast {
             resultMap.put(temperature.getKey(), temperature.getValue()+1);
         }
         return resultMap;
+    }
+
+    public double calculateTemperatureAverage() {
+        double temperatureAverage;
+        double temperatureSum = 0.0;
+        for (Map.Entry<String, Double> eachSensor : temperatures.getTemperatures().entrySet()) {
+            temperatureSum = temperatureSum + eachSensor.getValue();
+        }
+        temperatureAverage = temperatureSum / temperatures.getTemperatures().entrySet().size();
+        return temperatureAverage;
+    }
+
+    public double calculateMiddleValue() {
+        double value = 0.0;
+        List<Double> tempList = new ArrayList<>();
+        for (Map.Entry<String,Double> eachSensor : temperatures.getTemperatures().entrySet()) {
+            tempList.add(eachSensor.getValue());
+        }
+        Collections.sort(tempList);
+        if (tempList.size() % 2 == 0) {
+                double value1 = tempList.get(tempList.size() / 2 - 1);
+                double value2 = tempList.get(tempList.size() / 2);
+                double middleValue = (value1 + value2)/2;
+                return middleValue;
+            }else {
+            value = tempList.get(tempList.size()/2);
+        }
+        return value;
     }
 
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/weather/mock.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/weather/mock.java
@@ -1,3 +1,5 @@
+package com.kodilla.testing.weather;
+
 import com.kodilla.testing.weather.stub.Temperatures;
 import com.kodilla.testing.weather.stub.WeatherForecast;
 import org.junit.jupiter.api.Assertions;
@@ -36,5 +38,47 @@ class WeatherForecastTestSuite {
 
         //Then
         Assertions.assertEquals(5, quantityOfSensors);
+    }
+
+    @Test
+    void testTemperatureAverage() {
+        //Given
+        Map<String, Double> temperaturesMap = new HashMap<>();
+        //dummy data
+        temperaturesMap.put("Rzeszow", 20.2);
+        temperaturesMap.put("Krakow", 29.6);
+        temperaturesMap.put("Wroclaw", 36.5);
+        temperaturesMap.put("Warszawa", 17.1);
+        temperaturesMap.put("Gdansk", 22.3);
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
+
+        //When
+        double expectedAverage = (20.2+29.6+36.5+17.1+22.3)/5;
+        double receivedAverage = weatherForecast.calculateTemperatureAverage();
+
+        //Then
+        Assertions.assertEquals(expectedAverage, receivedAverage);
+    }
+
+    @Test
+    void testTemperatureMiddleValue() {
+        //Given
+        Map<String, Double> temperaturesMap = new HashMap<>();
+        //dummy data
+        temperaturesMap.put("Rzeszow", 15.0);
+        temperaturesMap.put("Krakow", 20.0);
+        temperaturesMap.put("Wroclaw", 18.0);
+        temperaturesMap.put("Warszawa", 19.0);
+        temperaturesMap.put("Gdansk", 22.0);
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
+
+        //When
+        double expectedValue = 19.0;
+        double receivedAverage = weatherForecast.calculateMiddleValue();
+
+        //Then
+        Assertions.assertEquals(expectedValue, receivedAverage);
     }
 }
