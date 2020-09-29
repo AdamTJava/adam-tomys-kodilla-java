2941135a3ed561b3eb6908bf099886db826fa115
AdamTJava <adamtomys@gmail.com>
2020-09-29 19:43:34 +0200

    kodilla-spring

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
 import java.util.stream.Stream;
 
 public class FileReader {
@@ -26,7 +27,7 @@ public class FileReader {
 
     public void readFile(final String fileName) throws FileReaderException {
         ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource(fileName).getFile());
+        File file = new File(Objects.requireNonNull(classLoader.getResource(fileName)).getFile());
 
         try (Stream<String> fileLines = Files.lines(Path.of(file.toURI()))) {// różnica pomiędzy getPath klasy Paths, a toURI klasy Path
             fileLines.forEach(System.out::println);
--- kodilla-exception/src/test/java/com/kodilla/exception/io/FileReaderTestSuite.java
+++ kodilla-exception/src/test/java/com/kodilla/exception/io/FileReaderTestSuite.java
@@ -15,7 +15,7 @@ class FileReaderTestSuite {
     }
 
     @Test
-    void whenFileDosentExistsReadFileShouldThrowException() {
+    void whenFileDoesntExistsReadFileShouldThrowException() {
         // given
         FileReader fileReader = new FileReader();
         String fileName = "nie_ma_takiego_pliku.txt";
@@ -30,7 +30,7 @@ class FileReaderTestSuite {
         // when & then
 //        assertAll(
 //                () -> assertThrows(FileReaderException.class, () -> fileReader.readFile("nie_ma_takiego_pliku.txt")),
-//                //() -> assertThrows(FileReaderException.class, () -> fileReader.readFile(null)),
+//                () -> assertThrows(FileReaderException.class, () -> fileReader.readFile(null)),
 //                () -> assertDoesNotThrow(() -> fileReader.readFile("names.txt"))
 //        );
     }
