a408e9e9923edd01fb02561ab5afeb7209365304
AdamTJava <adamtomys@gmail.com>
2020-10-05 19:27:30 +0200

    git fix

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -2,6 +2,7 @@ package com.kodilla.exception.io;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -27,15 +28,13 @@ public class FileReader {
 
     public void readFile(final String fileName) throws FileReaderException {
         ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(Objects.requireNonNull(classLoader.getResource(fileName)).getFile());
 
-        try (Stream<String> fileLines = Files.lines(Path.of(file.toURI()))) {// różnica pomiędzy getPath klasy Paths, a toURI klasy Path
+        try (Stream<String> fileLines = Files.lines(Path.of(classLoader.getResource(fileName).toURI()))) {// różnica pomiędzy getPath klasy Paths, a toURI klasy Path
             fileLines.forEach(System.out::println);
         } catch (Exception e) {
             throw new FileReaderException();
         } finally {
             System.out.println("I am gonna be here... always!");
         }
-        System.out.println(file.getPath());
     }
 }
--- kodilla-exception/src/test/java/com/kodilla/exception/io/FileReaderTestSuite.java
+++ kodilla-exception/src/test/java/com/kodilla/exception/io/FileReaderTestSuite.java
@@ -20,7 +20,7 @@ class FileReaderTestSuite {
         FileReader fileReader = new FileReader();
         String fileName = "nie_ma_takiego_pliku.txt";
         // when & then
-//        assertThrows(FileReaderException.class, () -> fileReader.readFile(fileName));
+        assertThrows(FileReaderException.class, () -> fileReader.readFile(fileName));
     }
 
     @Test
@@ -28,10 +28,10 @@ class FileReaderTestSuite {
         // given
         FileReader fileReader = new FileReader();
         // when & then
-//        assertAll(
-//                () -> assertThrows(FileReaderException.class, () -> fileReader.readFile("nie_ma_takiego_pliku.txt")),
-//                () -> assertThrows(FileReaderException.class, () -> fileReader.readFile(null)),
-//                () -> assertDoesNotThrow(() -> fileReader.readFile("names.txt"))
-//        );
+        assertAll(
+                () -> assertThrows(FileReaderException.class, () -> fileReader.readFile("nie_ma_takiego_pliku.txt")),
+                () -> assertThrows(FileReaderException.class, () -> fileReader.readFile(null)),
+                () -> assertDoesNotThrow(() -> fileReader.readFile("names.txt"))
+        );
     }
 }
